64d005220ecd9fd5065629413159c3e063e08842
Pranav <pb719@ic.ac.uk>
2021-05-19 10:36:13 +0100

    Add proof.go

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,1 +1,14 @@
 package blockchain
+
+// Proof Of Work Algorithm
+
+// 1. Take data from the block
+// 2. Create a counter (nonce) starting at 0
+// 3. Create a hash of the data plaus the nonce
+// 4. Check the hash to see if it meets the requirements
+
+// Requirements:
+// i) First few bytes must contain 0s
+
+const Difficulty = 12
+
