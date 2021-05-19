1afa2b785daa3d638076a0f5207102411b1a08c4
Pranav <pb719@ic.ac.uk>
2021-05-19 10:57:11 +0100

    Add proof of work alg

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,5 +1,14 @@
 package blockchain
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"math/big"
+)
+
 // Proof Of Work Algorithm
 
 // 1. Take data from the block
@@ -10,5 +19,71 @@ package blockchain
 // Requirements:
 // i) First few bytes must contain 0s
 
+// Difficulty affects the requirements
 const Difficulty = 12
 
+// ProofOfWork structure
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int // represents the requirement(s)
+}
+
+// NewProof does the proof-of-work work
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+
+	return &ProofOfWork{b, target}
+}
+
+// InitData initialises the data
+func (pow ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+			toHex(int64(nonce)),
+			toHex(int64(Difficulty)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	nonce := 0
+
+	var hash [32]byte
+	var intHash big.Int
+
+	for nonce < math.MaxInt64 {
+		data := pow.InitData(nonce)
+		hash = sha256.Sum256(data)
+
+        fmt.Printf("\r%x", hash)
+
+        intHash.Cmp(pow.Target) == -1 {
+            break
+        } else {
+            nonce++
+        }
+	}
+
+    fmt.Println()
+
+    retuurn nonce, hash[:]
+}
+
+// ====================== UTILITIES ======================
+
+func toHex(no int64) []byte {
+	buff := new(bytes.Buffer)
+
+	err := binary.Write(buff, binary.BigEndian, no)
+	if err != nil {
+		panic(err)
+	}
+
+	return buff.Bytes()
+}
