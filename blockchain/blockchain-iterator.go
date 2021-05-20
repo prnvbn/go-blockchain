d9c20b4e1492c065a5cf37e2e5a86e401407cb75
Pranav <pb719@ic.ac.uk>
2021-05-20 09:58:59 +0100

    ADD an iterator for BlockChain strict

---
+++ blockchain/blockchain-iterator.go
@@ -0,0 +1,42 @@
+package blockchain
+
+import "github.com/dgraph-io/badger"
+
+// BCIterator is an Iterator for block chain
+// TODO? Use interface
+type BCIterator struct {
+	CurrentHash []byte
+	Database    *badger.DB
+}
+
+// Iterator returns a BlockChainIterator for a blockchain
+func (chain *BlockChain) Iterator() *BCIterator {
+	return &BCIterator{
+		CurrentHash: chain.LastHash,
+		Database:    chain.Database,
+	}
+}
+
+// Next will give the next block
+func (iter *BCIterator) Next() *Block {
+	var block *Block
+
+	err := iter.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iter.CurrentHash)
+		HandleErr(err)
+		var encodedBlock []byte
+		item.Value(func(val []byte) error {
+			encodedBlock = val
+			return nil
+		})
+
+		block = Deserialize(encodedBlock)
+
+		return nil
+	})
+	HandleErr(err)
+
+	iter.CurrentHash = block.PrevHash
+
+	return block
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-blockchain
 
 go 1.16
+
+require github.com/dgraph-io/badger v1.6.2 // indirect
